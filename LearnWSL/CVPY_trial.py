211b7fa1f4f3577744f59b480f4ac55606eafc3e
Luigi946 <luigi.russo@unisannio.it>
2022-06-01 18:35:40 -0700

    first commit

---
+++ LearnWSL/CVPY_trial.py
@@ -0,0 +1,14 @@
+import cvxpy as cp
+from numpy import integer
+# Create two scalar variables
+x=cp.Variable()
+y=cp.Variable()
+Constraints=[x+y==1, x-y>=1]
+obj=cp.Minimize((x-y)**2)
+prob=cp.Problem(obj,Constraints)
+prob.solve()
+print("status",prob.status)
+print("optimal value",prob.value)
+print("optimal var",x.value,y.value)
+z=cp.Variable(1,boolean=True)
+a=1
